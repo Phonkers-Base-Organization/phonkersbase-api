c8a12feb12642f98a001f3ebec70ea95d92a29e9
Artem <arvlas78@gmail.com>
2026-06-20 20:59:56 +0300

    feat: made artists be sorted by updated_at (which also defaults to created_at) after being sorted by priority

--- internal/repository/artist.go
+++ internal/repository/artist.go
@@ -144,7 +144,7 @@ func (r *ArtistRepo) GetAll(ctx context.Context, p domain.ListArtistsParams) (*d
 		orderParts = append(orderParts, "a.total_priority "+safeDir(p.ByListen))
 	}
 	if len(orderParts) == 0 {
-		orderParts = []string{"a.total_priority DESC"}
+		orderParts = []string{"a.total_priority DESC, a.updated_at DESC"}
 	}
 	orderParts = append(orderParts, "a.id ASC")
 	orderClause := "ORDER BY " + strings.Join(orderParts, ", ")
